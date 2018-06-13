7cf55efd5964c973966bee21beb28c6e07eb67a6
Max <gusstorm132@gmail.com>
2018-06-13 10:46:56 -0700

    tests are working

---
+++ challenges/LinkedLists/LinkList/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using LinkList;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            LinkedList ll = 
+        }
+    }
+}
