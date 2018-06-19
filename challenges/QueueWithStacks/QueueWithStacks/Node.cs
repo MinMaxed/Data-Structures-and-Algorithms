6a23f1e4f4f3e7520789a7891ef1fac3d9ed546c
Max <gusstorm132@gmail.com>
2018-06-18 21:02:59 -0700

    committing for branch switch

---
+++ challenges/QueueWithStacks/QueueWithStacks/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueWithStacks
+{
+    public class Node
+    {
+        //basic node
+        public int Value { get; set; }
+
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+    }
+}
