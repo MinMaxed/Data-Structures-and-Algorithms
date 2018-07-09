b00067bdfea0bb3ece9161cb7474ba7cf57f13b0
Max <gusstorm132@gmail.com>
2018-07-09 09:47:18 -0700

    scaffolding out the class files

---
+++ data-structures/Graphs/Graphs/Graph.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphs
+{
+    public class Graph
+    {
+        public Node Node { get; set; }
+
+        public void AddEdge()
+        {
+        }
+
+        public List<Node> GetNodes()
+        {
+
+        }
+
+        public List<Node> GetNeighbors()
+        {
+
+        }
+
+        public int Size()
+        {
+
+        }
+
+        public List<Node> BreadthFirst(Node root)
+        {
+            List<Node> order = new List<Node>();
+            Queue<Node> breadth = new Queue<Node>();
+            breadth.Enqueue(root);
+
+            while (breadth.TryPeek(out root))
+            {
+                Node front = breadth.Dequeue();
+                order.Add(front);
+
+                foreach (Node child in front.Children)
+                {
+                    if (!child.Visited)
+                    {
+                        child.Visited = true;
+                        breadth.Enqueue(child);
+                    }
+                }
+            }
+            return order;
+        }
+
+
+
+
+    }
+}
---
+++ data-structures/Graphs/Graphs/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphs
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public bool Visited { get; set; } = false;
+        public Node Children { get; set; }
+        public Node Edge { get; set; }
+
+    }
+}
