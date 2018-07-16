fdde77ae5819de20823439721ce9215dc243533e
Max <gusstorm132@gmail.com>
2018-07-16 11:28:57 -0700

    added asciivalue finder

--- data-structures/HashTables/HashTables/HashTables/HashTable.cs
+++ data-structures/HashTables/HashTables/HashTables/HashTable.cs
@@ -6,9 +6,21 @@ namespace HashTables
 {
     class HashTable
     {
+        public LinkedList<> Bucket;
 
+        public int asciiValue (string key)
+        {
+            int value = 0;
+            for (int i = 0; i < key.Length; i++)
+            {
+                value += key[i];
+            }
 
-        public void Add(int key, int value)
+            return value;
+
+        }
+
+        public void Add(string key, int value)
         {
 
         }
---
+++ data-structures/HashTables/HashTables/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestCollision()
+        {
+
+        }
+
+        [Fact]
+        public void TestContains()
+        {
+
+        }
+
+        [Fact]
+        public void TestInsertion()
+        {
+
+        }
+    }
+}
